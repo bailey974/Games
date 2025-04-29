7cf252fd68f9c2a4adab735486eebf3f706bb6d3
bailey974 <bscanlan.scanlan8@gmail.com>
2025-04-29 18:33:17 +0100

    started player class

---
+++ Pong/game.java
@@ -0,0 +1,18 @@
+class Player {
+    //current position
+    Pvector position;
+    // Movement direction
+    float yDir;
+    // Size
+    float w = 10;
+    float h = 25;
+    // boundaries
+    float b = 15;
+    Public Player(float x, float yDir) {
+        position = new Pvector(x, y);
+        this.yDir = yDir;
+    }
+
+    // Settler Methods
+    Public void s
+}
